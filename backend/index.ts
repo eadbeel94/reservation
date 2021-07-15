4afe1f631c6bdd9f3844a3c56c311e12e39d00f9
Adbeel Estrada <eadbeel94@outlook.com>
2021-07-15 12:22:49 -0500

    initialize project

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { join } from 'path';
+import cors from 'cors';
+
+//--------------------------- Config options ---------------------------
+const app= express();
+
+app.set('PORT' , process.env.PORT || '3001' );
+
+//--------------------------- Global middlewares ---------------------------
+app.use( cors() );
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+//--------------------------- Routes ---------------------------
+app.use( '/' , ( req, res ) => res.send('hello') );
+
+//--------------------------- Static files ---------------------------
+//app.use( express.static( join(__dirname, '../build') ) );
+
+//--------------------------- Errors ---------------------------
+
+
+//--------------------------- Initialize server ---------------------------
+app.listen( app.get('PORT') , ()=> console.log(`Server on port ${ app.get('PORT') }`) );
